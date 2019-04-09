7856bd5ae49461e015cdfb06e01e4bde2cd330f4
kriviears <kriviears@gmail.com>
2019-04-09 16:58:34 -0400

    added data for the tastemaker app

--- data.js
+++ data.js
@@ -23,4 +23,14 @@ const projects = [
     live: '#home',
     git: 'https://github.com/Kriviears/final-portfolio',
   },
+  {
+    name: 'Taste making app',
+    description: 'asks what you like and uses the qloo api to make suggestions on what else you might like. Coded in html, css, and jquery',
+    pic: {
+      src: 'images/tastemaker.jpeg',
+      alt: 'Tastemaker app'
+    },
+    live: 'https://kriviears.github.io/tastemaker-app/',
+    git: 'https://github.com/Kriviears/tastemaker-app',
+  },
 ];
